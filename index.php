330abaed808dae75c4e9a1a635ffacfdd9f5ddfc
chalosalvador <chalosalvador@gmail.com>
2015-07-14 13:16:37 -0500

    BDD

--- index.php
+++ index.php
@@ -1,2 +1,19 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 echo "Hola!! :) This is it!!!";
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+if($conn->connect_error) 
+  die($conn->connect_error);
+
+$result=$conn->query("SELECT * FROM clasicos");
+if(!$result) die($conn->error);
+
+$num_rows = $result->num_rows;
